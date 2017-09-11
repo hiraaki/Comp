4466faf87ad56cc787161ea4cba2380dbfb2cc7f
mhi <mauricio_ishida@hotmail.com>
2017-09-11 12:13:38 -0300

    vai

---
+++ Comp/Class/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comp.Class
+{
+    class Token
+    {
+        public string token { get; set; }
+        public string type { get; set; }
+    }
+    
+}
